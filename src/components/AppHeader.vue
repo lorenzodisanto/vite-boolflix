ec4885d0a63bcb3f675df44e77e30201aeab9f30
lorenzodisanto <lore.disanto95@gmail.com>
2024-02-15 23:44:29 +0100

    feat: add app header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,24 +4,58 @@ export default {
   data() {
     return {
       store,
+      searchTitle: "",
     };
   },
+
+  methods: {
+    upSearchTitle() {
+      store.searchTitle = this.searchTitle;
+    },
+  },
+
+  emits: ["click-contents"],
 };
 </script>
 
 <template>
-  <!-- <div class="input-group mb-3">
-    <input
-      type="text"
-      class="form-control"
-      placeholder="Scegli film"
-      aria-label="Scegli film"
-      aria-describedby="button"
-    />
-    <button class="btn btn-outline-secondary" type="button" id="button">
-      Search
-    </button>
-  </div> -->
+  <header>
+    <div class="container header-section">
+      <div>
+        <h1 class="text-danger">{{ store.title }}</h1>
+      </div>
+
+      <!-- search bar -->
+      <div class="input-group mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Inizia Ricerca"
+          aria-label="Inizia Ricerca"
+          aria-describedby="button"
+          @keyup="upSearchTitle()"
+          @keyup.enter="$emit('click-contents')"
+          v-model="searchTitle"
+        />
+        <button
+          @click="$emit('click-contents')"
+          class="btn btn-outline-secondary"
+          type="button"
+          id="button"
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+  background-color: black;
+  height: 120px;
+  position: sticky;
+  top: 0;
+  z-index: 1;
+}
+</style>
