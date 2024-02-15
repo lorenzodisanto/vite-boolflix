91947059f84b23197d705db3cd1a2eab2725fcb7
lorenzodisanto <lore.disanto95@gmail.com>
2024-02-15 23:44:09 +0100

    feat: add app main

---
+++ src/components/AppMain.vue
@@ -0,0 +1,18 @@
+<script>
+import { store } from "../store";
+import AppCard from "./AppCard.vue";
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+  components: { AppCard },
+};
+</script>
+
+<template>
+  <div class="container"><app-card></app-card></div>
+</template>
+
+<style lang="scss" scoped></style>
